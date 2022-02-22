f3b42bafe469d2332d7d3005480fac82764dc8ba
Matt Harrison <MattHarrison92@gmail.com>
2022-02-22 15:17:21 -0600

    add full stack project to portfolio

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,78 +1,121 @@
-import React from 'react'
-import Matt2 from '../img/IMG_0940.jpeg'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import React from 'react';
+import Matt2 from '../img/IMG_0940.jpeg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 // Framer Motion
-import { motion } from 'framer-motion'
-import { pageAnimation } from '../animation'
-import { Link } from 'react-router-dom'
+import { motion } from 'framer-motion';
+import { pageAnimation } from '../animation';
+import { Link } from 'react-router-dom';
 // import { photoAnim } from '../animation'
 
-
 const AboutSection = () => {
+  const titleAnim = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1, transition: { duration: 1 } },
+  };
 
-    const titleAnim = {
-        hidden: { opacity: 0 },
-        show: { opacity: 1, transition: { duration: 1 } }
-    }
+  const headlineAnim = {
+    hidden: { y: 200 },
+    show: { y: 0, transition: { duration: 1 } },
+  };
+  const container = {
+    hidden: { x: 100, opacity: 0 },
+    show: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.3,
+        ease: 'easeOut',
+        staggerChildren: 0.2,
+        when: 'beforeChildren',
+      },
+    },
+  };
 
-    const headlineAnim = {
-        hidden: { y: 200 },
-        show: { y: 0, transition: { duration: 1 } }
-    }
-    const container = {
-        hidden: { x: 100, opacity: 0 },
-        show: { x: 0, opacity: 1, transition: { duration: .3, ease: "easeOut", staggerChildren: .20, when: 'beforeChildren' } }
-    }
+  return (
+    <motion.div
+      exit='exit'
+      variants={pageAnimation}
+      initial='hidden'
+      animate='enter'
+    >
+      <motion.div
+        className='about-layout'
+        variants={container}
+        initial='hidden'
+        animate='show'
+      >
+        <div className='about-description'>
+          <div className='title'>
+            <div className='hide'>
+              <motion.h2 variants={headlineAnim}>Hi, I'm Matt</motion.h2>
+            </div>
+            <div className='hide'>
+              <motion.h2 variants={headlineAnim}>
+                <span> Full Stack</span> Developer
+              </motion.h2>
+            </div>
+          </div>
+          <motion.p variants={titleAnim} style={{ color: 'white' }}>
+            This Portfolio was built with{' '}
+            <span style={{ color: '#61dafb' }}>React</span>
+          </motion.p>
+          <div className='buttons'>
+            <Link to={'/projects'}>
+              <motion.button
+                whileHover={{ y: -10, transition: { duration: 0.02 } }}
+                variants={titleAnim}
+                style={{ marginRight: '.5rem' }}
+              >
+                Projects
+              </motion.button>
+            </Link>
+            <Link to={'/resume'}>
+              <motion.button
+                whileHover={{ y: -10, transition: { duration: 0.02 } }}
+                variants={titleAnim}
+                style={{ marginRight: '.5rem' }}
+              >
+                Resume
+              </motion.button>
+            </Link>
+            <div className='icons'>
+              <motion.a
+                whileHover={{ y: -10, transition: { duration: 0.3 } }}
+                href='https://github.com/Fittocode'
+                className='brand-icon'
+              >
+                <motion.div className='brand-icon' variants={titleAnim}>
+                  <FontAwesomeIcon size='3x' icon={faGithub} />
+                </motion.div>
+              </motion.a>
+              <motion.a
+                whileHover={{ y: -10, transition: { duration: 0.3 } }}
+                href='https://www.linkedin.com/in/matt-harrison-5697b363/'
+                className='brand-icon'
+              >
+                <motion.div className='brand-icon' variants={titleAnim}>
+                  <FontAwesomeIcon
+                    className='brand-icon'
+                    size='3x'
+                    icon={faLinkedin}
+                  />
+                </motion.div>
+              </motion.a>
+            </div>
+          </div>
+        </div>
+        <div className='about-image'>
+          <motion.img
+            variants={titleAnim}
+            src={Matt2}
+            alt='Matt Harrison portrait'
+          />
+        </div>
+      </motion.div>
+    </motion.div>
+  );
+};
 
-    return (
-        <motion.div exit="exit" variants={pageAnimation} initial='hidden' animate="enter">
-            <motion.div className="about-layout" variants={container} initial="hidden" animate="show">
-                <div className="about-description">
-                    <div className="title">
-                        <div className="hide">
-                            <motion.h2 variants={headlineAnim}>Hi, I'm Matt</motion.h2>
-                        </div>
-                        <div className="hide">
-                            <motion.h2 variants={headlineAnim}>
-                                <span> Front-end</span> Developer
-                            </motion.h2>
-                        </div>
-                    </div>
-                    <motion.p variants={titleAnim} style={{color: 'white'}}>This Portfolio was built with <span style={{ color: '#61dafb' }}>React</span></motion.p>
-                    <div className="buttons">
-                            <Link to={'/projects'}>
-                        <motion.button whileHover={{ y: -10, transition: { duration: .02 } }} variants={titleAnim} style={{ marginRight: '.5rem' }}>
-                                Projects
-                        </motion.button>
-                            </Link>
-                            <Link to={'/resume'}>
-                        <motion.button whileHover={{ y: -10, transition: { duration: .02 } }} variants={titleAnim} style={{ marginRight: '.5rem' }}>
-                                Resume
-                        </motion.button>
-                            </Link>
-                        <div className="icons">
-                            <motion.a whileHover={{ y: -10, transition: { duration: .3 } }} href="https://github.com/Fittocode" className="brand-icon">
-                                <motion.div className="brand-icon" variants={titleAnim}>
-                                    <FontAwesomeIcon size="3x" icon={faGithub} />
-                                </motion.div>
-                            </motion.a>
-                            <motion.a whileHover={{ y: -10, transition: { duration: .3 } }} href="https://www.linkedin.com/in/matt-harrison-5697b363/" className="brand-icon">
-                                <motion.div className="brand-icon" variants={titleAnim}>
-                                    <FontAwesomeIcon className="brand-icon" size="3x" icon={faLinkedin} />
-                                </motion.div>
-                            </motion.a>
-                        </div>
-                    </div>
-                </div>
-                <div className="about-image">
-                    <motion.img variants={titleAnim} src={Matt2} alt="Matt Harrison portrait" />
-                </div>
-            </motion.div>
-        </motion.div>
-    )
-}
-
-
-export default AboutSection
+export default AboutSection;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,65 +1,116 @@
-import React from 'react'
-import JournalGif from '../img/Journal-app.gif'
-import musicplayerGif from '../img/musicplayer.gif'
+import React from 'react';
+import JournalGif from '../img/Journal-app.gif';
+import musicplayerGif from '../img/musicplayer.gif';
+import SocialMediaApp from '../img/social-media-app.png';
 // Animations
-import { motion } from 'framer-motion'
-import { pageAnimation } from '../animation'
-import { images } from "../images";
+import { motion } from 'framer-motion';
+import { pageAnimation } from '../animation';
+import { images } from '../images';
 
 const transition = { duration: 0.2, ease: [0.43, 0.13, 0.23, 0.96] };
 
 const thumbnailVariants = {
-    initial: { scale: 0.9, opacity: 1, size: 1 },
-    enter: { scale: 1, opacity: 1, transition },
+  initial: { scale: 0.9, opacity: 1, size: 1 },
+  enter: { scale: 1, opacity: 1, transition },
 };
 
 const frameVariants = {
-    hover: { scale: 1.01, opacity: 1, boxShadow: '5px 5px 15px -5px rgb(255, 255, 255, .7)'}
+  hover: {
+    scale: 1.01,
+    opacity: 1,
+    boxShadow: '5px 5px 15px -5px rgb(255, 255, 255, .7)',
+  },
 };
 
 const Thumbnail = () => (
-    <motion.div className="frame" variants={thumbnailVariants}>
-        <div className="projects-items">
-            <motion.p className="projects-title">A fully functioning Music Player app, built with React and styled with Sass. </motion.p>
-            <motion.div
-                whileHover="hover"
-                variants={frameVariants}
-                transition={transition}
-            >
-                <a href="https://gallant-heyrovsky-e92bf2.netlify.app/" target="tab"><img className="thumbnail" src={musicplayerGif} alt="This is an animated gif of my Journal App"/></a>
-            </motion.div>
-        </div>
-        <div className="project-items">
-            <motion.p className="projects-title">A Twitter-like Journal application, built with React and styled with Sass. Still being developed. Currently only client-side. </motion.p>
-            <motion.div
-                whileHover="hover"
-                variants={frameVariants}
-                transition={transition}
-            >
-                <a href="https://zealous-kepler-ab4529.netlify.app/" target="tab"><img className="thumbnail" src={JournalGif} alt="This is an animated gif of my Journal App"/></a>
-            </motion.div>
-        </div>
-    </motion.div>
+  <motion.div className='frame' variants={thumbnailVariants}>
+    <div className='projects-items'>
+      <motion.p className='projects-title'>
+        A Full Stack, Express/Node.js Social Media App connected to MongoDB
+        Atlas, authenticated with Passport, and deployed to Heroku via Docker{' '}
+      </motion.p>
+      <motion.div
+        whileHover='hover'
+        variants={frameVariants}
+        transition={transition}
+      >
+        <a
+          href='https://mattharrisondev-social-app.herokuapp.com/login'
+          target='tab'
+        >
+          <img
+            className='social-pic'
+            src={SocialMediaApp}
+            alt='This is an image of my Social Media App'
+          />
+        </a>
+      </motion.div>
+    </div>
+    <div className='projects-items'>
+      <motion.p className='projects-title'>
+        A fully functioning Music Player app, built with React and styled with
+        Sass.{' '}
+      </motion.p>
+      <motion.div
+        whileHover='hover'
+        variants={frameVariants}
+        transition={transition}
+      >
+        <a href='https://gallant-heyrovsky-e92bf2.netlify.app/' target='tab'>
+          <img
+            className='thumbnail'
+            src={musicplayerGif}
+            alt='This is an animated gif of my Journal App'
+          />
+        </a>
+      </motion.div>
+    </div>
+    <div className='project-items'>
+      <motion.p className='projects-title'>
+        A Twitter-like Journal application, built with React and styled with
+        Sass. Still being developed. Currently only client-side.{' '}
+      </motion.p>
+      <motion.div
+        whileHover='hover'
+        variants={frameVariants}
+        transition={transition}
+      >
+        <a href='https://zealous-kepler-ab4529.netlify.app/' target='tab'>
+          <img
+            className='thumbnail'
+            src={JournalGif}
+            alt='This is an animated gif of my Journal App'
+          />
+        </a>
+      </motion.div>
+    </div>
+  </motion.div>
 );
 
 export const Projects = () => {
-    return (
-        <motion.div className="projects-layout" exit="exit" variants={pageAnimation} initial='hidden' animate="enter">
-            <div className="gallery">
-                <motion.div
-                    className="thumbnails"
-                    initial="initial"
-                    animate="enter"
-                    exit="exit"
-                    variants={{ exit: { transition: { staggerChildren: 0.1 } } }}
-                >
-                    {images.map((id, i) => (
-                        <Thumbnail key={id} id={id} i={i} />
-                    ))}
-                </motion.div>
-            </div>
-        </motion.div >
-    )
-}
+  return (
+    <motion.div
+      className='projects-layout'
+      exit='exit'
+      variants={pageAnimation}
+      initial='hidden'
+      animate='enter'
+    >
+      <div className='gallery'>
+        <motion.div
+          className='thumbnails'
+          initial='initial'
+          animate='enter'
+          exit='exit'
+          variants={{ exit: { transition: { staggerChildren: 0.1 } } }}
+        >
+          {images.map((id, i) => (
+            <Thumbnail key={id} id={id} i={i} />
+          ))}
+        </motion.div>
+      </div>
+    </motion.div>
+  );
+};
 
-export default Projects
+export default Projects;
